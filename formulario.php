0fc3316cfce88dbbb03035badb65febadf4a5002
SilviaLenFer <sillendi@ucm.es>
2018-02-26 13:44:54 +0100

    Empezando contacto.html y su php
    Formulario de contacto cutre

---
+++ formulario.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+
+</head>
+<body>
+
+<?php
+	
+	$nombre = $_POST["nom"];
+	echo "Nombre: $nombre "; 
+
+	$contra = $_POST["cont"];
+	echo "Contra: $contra";
+
+	//if ($_POST["nom"])){ // Aqui comprobaremos si existe o no ... etc
+	//}
+	
+
+?>
+
+
+</body>
+</html>
